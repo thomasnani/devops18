0d10bc5315a64168ee6e88aee316e2c16464833e
thomasnani <128666177+thomasnani@users.noreply.github.com>
2025-03-24 04:33:15 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "mustafashaik77.devops.project.bucket"
+  bucket = "awstrustedadvlogs2342.devops.project.bucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
@@ -25,7 +25,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "us-east-1"
-bucket = "mustafashaik77.devops.project.bucket"
+bucket = "awstrustedadvlogs2342.devops.project.bucket"
 key = "prod/terraform.tfstate"
 }
 }
